fb646b8115396a7f7c3edfd5d0d5af0b986b5b14
Cyrus <cdbalsara@icloud.com>
2017-02-13 14:37:46 +0000

    tested room has guests in it

--- room.rb
+++ room.rb
@@ -1,9 +1,15 @@
 class Room
-  attr_accessor :room_name
+  attr_accessor :room_name, :guestlist, :playlist
 
-  def initialize(name)
+  def initialize(name, guests, songs)
     @room_name = name
- 
+    @guestlist = guests
+    @playlist = songs
+  end 
+
+  def calculate_guest_total
+    result = @guestlist.map{|guest| guest}
+    result.length
   end 
 
 
--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -1,15 +1,34 @@
 require ('minitest/autorun')
 require('minitest/rg')
 require_relative('../room')
+require_relative('../guest')
+require_relative('../song')
 
 class RoomTest < Minitest::Test
 
   def setup
-    @room1 = Room.new("Karaoke Room")
+    guest1 = Guest.new("Homer","Simpson")
+    guest2 = Guest.new("Tony", "Soprano")
+    guest3 = Guest.new("Charles", "Xavier")
+    guest4 = Guest.new("Frodo", "Baggins")
+    guestlist = [guest1, guest2, guest3, guest4]
+
+    song1 = Song.new("Metallica", "For Whom the Bell Tolls")
+    song2 = Song.new("AC/DC", "Whole Lotta Rosie")
+    song3 = Song.new("Pink Floyd", "Coming Back to Life")
+    playlist = [song1, song2, song3]
+
+    @room1 = Room.new("Karaoke Room", guestlist, playlist)
+   
   end 
 
   def test_room_has_name
     assert_equal('Karaoke Room', @room1.room_name)
+  end 
+
+  def test_room_has_guests
+    number_of_occupants = @room1.calculate_guest_total()
+    assert_equal(4, number_of_occupants)
   end 
 
 
