f813d22e433be6d5bef1ed189702cf4a3be7e3c1
Cyrus <cdbalsara@icloud.com>
2017-02-13 15:54:21 +0000

    tested that poeple can beed added to the empty karaoke room, and then removed one at a time

--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -20,33 +20,42 @@ class RoomTest < Minitest::Test
     playlist = [song1, song2, song3]
     playlist2 = []
 
-    @room1 = Room.new("Karaoke Room", guestlist, playlist)
+    @room1 = Room.new("Bar Room", guestlist, playlist)
     @room2 = Room.new("Karaoke Room", guestlist2, playlist2)
    
   end 
 
   def test_room_has_name
-    assert_equal('Karaoke Room', @room1.room_name)
+    assert_equal('Bar Room', @room1.room_name)
+    assert_equal('Karaoke Room', @room2.room_name)
   end 
 
-  def test_room_starts_with_no_guests
+  def test_karaoke_room_empty_at_start
     assert_equal(0, @room2.guestlist.length)
 
   end 
 
-  def test_room_has_guests
+  def test_bar_has_guests
     number_of_occupants = @room1.calculate_guest_total()
     assert_equal(4, number_of_occupants)
   end 
 
-  def test_add_guest_to_room
+  def test_add_guest_to_karaoke_room
     guests_added = @room2.addGuest("Cyrus", "Balsara")
     assert_equal(1, guests_added)
   end 
 
-  def test_remove_guest_from_room
+  def test_remove_guest_from_bar_room
     remaining_guests = @room1.removeGuest()
     assert_equal(3, remaining_guests)
+  end 
+
+  def test_remove_guests_from_karaoke_room
+    @room2.addGuest("A", "Guest")
+    @room2.addGuest("Another", "Guest")
+    @room2.removeGuest()
+    assert_equal(1, @room2.guestlist.length)
+
   end 
 
   def test_room_starts_with_no_songs
